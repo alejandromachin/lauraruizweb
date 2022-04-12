1d8d41225aa61eafaeeb24c80a95e18b115e443a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-12 16:21:08 +0200

    added routes on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path="/gallery" element={<ProjectGallery />} />
+        <Route path="/list" element={<ProjectList />} />
+      </Routes>
     </>
   );
 }
