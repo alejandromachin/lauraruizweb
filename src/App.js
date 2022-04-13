72e17b5bbfa737ebe6258a1722a11a16c8619cb9
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-13 16:32:44 +0200

    added home page and project list page to routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home/Home";
+import ProjectList from "./pages/ProjectList/ProjectList";
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/gallery" element={<ProjectGallery />} />
-        <Route path="/list" element={<ProjectList />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/projectlist" element={<ProjectList />} />
       </Routes>
     </>
   );
