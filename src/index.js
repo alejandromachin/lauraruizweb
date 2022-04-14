d427cc099ddd85694b95815ddec9eb4b5270fd64
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-14 20:05:57 +0200

    added styles to heading on infoCard

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "@fontsource/inter";
+import "@fontsource/inter/300.css";
 
 ReactDOM.render(
   <React.StrictMode>
