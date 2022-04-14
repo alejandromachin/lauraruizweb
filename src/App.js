85acd2cd1b85bfbbded2fd7ad99ee91922d5da32
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-14 16:55:28 +0200

    added infoCard component and rendered in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import InfoCard from "./components/InfoCard/InfoCard";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home/Home";
 import ProjectList from "./pages/ProjectList/ProjectList";
 
 function App() {
   return (
-    <>
+    <div>
       <Navigation />
+      <InfoCard />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/projectlist" element={<ProjectList />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -1,4 +1,9 @@
 const InfoCard = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Laura Ruiz Penacho</h1>
+      <p>As an assistant director, I enable the creative team to flourish. </p>
+    </>
+  );
 };
 export default InfoCard;
