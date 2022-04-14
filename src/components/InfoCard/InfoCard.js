34dffcceeb3853e483a45f94fda116d8cc4e3f40
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-14 20:32:45 +0200

    added styles to text on infoCard

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -3,7 +3,10 @@ const InfoCard = () => {
     <div className="infoCard-container">
       <h1 className="infoCard-container__title">Laura Ruiz Penacho</h1>
       <p className="infoCard-container__text">
-        As an assistant director, I enable the creative team to flourish.{" "}
+        As an assistant <br />
+        director, I enable
+        <br /> the creative team <br />
+        to flourish.
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "@fontsource/inter";
 import "@fontsource/inter/300.css";
+import "@fontsource/inter/200.css";
 
 ReactDOM.render(
   <React.StrictMode>
