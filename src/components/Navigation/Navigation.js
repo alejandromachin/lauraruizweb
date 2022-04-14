a1e9c11e8db2df2b639ed41332b716a5e4c94426
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-14 14:01:35 +0200

    added currentPage class depending of current page to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const currentPage = useLocation();
+
   return (
     <>
       <ul className="navigation">
         <li>
-          <Link to={"/home"}>Gallery</Link>
+          <Link
+            to={"/home"}
+            className={currentPage.pathname === "/home" ? "currentPage" : " "}
+          >
+            Gallery
+          </Link>
         </li>
         <li>
-          <Link to={"/projectlist"}>List</Link>
+          <Link
+            to={"/projectlist"}
+            className={
+              currentPage.pathname === "/projectlist" ? "currentPage" : " "
+            }
+          >
+            List
+          </Link>
         </li>
       </ul>
     </>
