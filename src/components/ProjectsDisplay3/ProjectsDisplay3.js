a1cf92c4318128354cdf681f1880e3b878a66a69
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-04-21 17:47:30 +0200

    added images to render on flex box grid

---
+++ src/components/ProjectsDisplay3/ProjectsDisplay3.js
@@ -0,0 +1,26 @@
+const ProjectsDisplay3 = () => {
+  return (
+    <div className="projects-container">
+      <img className="" src="/images/12_octubre.webp" alt="" />
+      <img className="" src="/images/uterque.webp" alt="" />
+      <img className="" src="/images/soy_rebelde.webp" alt="" />
+      <img className="" src="/images/she_knows.webp" alt="" />
+      <img className="" src="/images/injubilables.webp" alt="" />
+      <img className="" src="/images/live_more_now.webp" alt="" />
+      <img className="" src="/images/wolkswagen_cady.webp" alt="" />
+      <img className="" src="/images/digues_el_que_penses.webp" alt="" />
+      <img className="" src="/images/mo_aitana.webp" alt="" />
+      <img className="" src="/images/fotocasa.webp" alt="" />
+      <img className="" src="/images/future_manifesto.webp" alt="" />
+      <img className="" src="/images/colacao.webp" alt="" />
+      <img className="" src="/images/light_free.webp" alt="" />
+      <img className="" src="/images/el_verano_que_merecemos.webp" alt="" />
+      <img className="" src="/images/mil_y_una_noches.webp" alt="" />
+      <img className="" src="/images/ya_no_vales.webp" alt="" />
+      <img className="" src="/images/fucking_money_man.webp" alt="" />
+      <img className="" src="/images/sen_senra.webp" alt="" />
+    </div>
+  );
+};
+
+export default ProjectsDisplay3;
