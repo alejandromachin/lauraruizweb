a695da9bbe405ee054890682888d899198137995
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-10 09:12:39 +0200

    working with static images instead of parallax

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,6 @@
-// import { Preload, Scroll, ScrollControls } from "@react-three/drei";
-// import { Suspense } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import ProjectsDisplay from "../../components/ProjectsDisplay/ProjectsDisplay";
-// import ProjectsDisplay2 from "../../components/ProjectsDisplay2/ProjectsDisplay2";
-import ProjectsDisplay3 from "../../components/ProjectsDisplay3/ProjectsDisplay3";
+import ProjectsDisplay from "../../components/ProjectsDisplay/ProjectsDisplay";
 
 const Home = () => {
-  return (
-    <>
-      {/* <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
-        <Suspense fallback={null}>
-          <ScrollControls damping={4} pages={3}>
-            <Scroll>
-              <ProjectsDisplay />;
-            </Scroll>
-          </ScrollControls>
-          <Preload />
-        </Suspense>
-      </Canvas> */}
-      <ProjectsDisplay3 />
-    </>
-  );
+  return <ProjectsDisplay />;
 };
 export default Home;
