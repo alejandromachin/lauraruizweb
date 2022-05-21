2a9d32d5677b2d81d4dd6d4cc9fea1e1ddfbee80
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-21 19:22:34 +0200

    moved infoLayer to the back of the image and changed sizes of images

--- src/styles/defaultStyles.js
+++ src/styles/defaultStyles.js
@@ -1,3 +1,3 @@
-export const smallImage = { width: "407px", height: "300px" };
+export const smallImage = { width: "307px", height: "200px" };
 export const mediumImage = { width: "546px", height: "397px" };
-export const largeImage = { width: "824px", height: "600px" };
+export const largeImage = { width: "724px", height: "500px" };
