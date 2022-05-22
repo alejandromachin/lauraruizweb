5c4253a836c9d01eb733ebb1a79b86b0037281f4
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-22 10:39:44 +0200

    added brackground color prop to projectCard container to render color

--- src/styles/ProjectsStyles.js
+++ src/styles/ProjectsStyles.js
@@ -41,7 +41,7 @@ const ProjectsContainer = styled.div`
 
 export const ProjectCardContainer = styled.div`
   position: relative;
-  background-color: red;
+  background-color: ${(props) => props.backgroundColor};
   display: flex;
   border-radius: 20px;
 
