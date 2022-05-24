d2da20ab42897cbd803d8a499c99cd7a7f1239c8
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-24 18:06:43 +0200

    added a projectListCard component to show info on list page

---
+++ src/components/ProjectListCard/ProjectListCard.js
@@ -0,0 +1,30 @@
+const ProjectListCard = ({
+  project: {
+    name,
+    data,
+    client,
+    director,
+    producer,
+    image,
+    imageSize,
+    backgroundColor,
+  },
+}) => {
+  return (
+    <ProjectCardContainer backgroundColor={backgroundColor}>
+      <div className="projectCard__text">
+        <ul>
+          <li>{name}</li>
+          <li>{data}</li>
+          <li>Client: {client}</li>
+          <li>Director: {director}</li>
+          <li>Producer: {producer}</li>
+          <li>PLAY</li>
+        </ul>
+      </div>
+      <img src={`images/${image}`} alt={name} className={`${imageSize}Image`} />
+    </ProjectCardContainer>
+  );
+};
+
+export default ProjectListCard;
