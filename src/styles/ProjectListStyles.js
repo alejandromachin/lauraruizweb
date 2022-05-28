827a74fd1f78f295f7f74c08e87c451b1195a505
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-28 11:51:02 +0200

    added font styles to projectListCard

--- src/styles/ProjectListStyles.js
+++ src/styles/ProjectListStyles.js
@@ -1,27 +1,39 @@
 import styled from "styled-components";
 
 export const ProjectListCardContainer = styled.div`
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Helvetica, sans-serif;
   width: 90%;
-  height: 120px;
+  height: 110px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   border-top: 1px solid #f45c54;
+
   .projectListCard__title {
+    display: flex;
+    flex-direction: column;
+    padding-top: 20px;
     font-size: 10px;
-    width: 30%;
     height: 100%;
+
+    h2 {
+      margin-top: 0;
+      margin-bottom: 0;
+      font-weight: 200;
+      font-size: 12px;
+    }
     p {
       font-family: Times, serif;
       font-style: italic;
+      margin-top: 0;
     }
   }
   .projectListCard__info {
-    font-size: 10px;
+    font-size: 12px;
     width: 30%;
     height: 100%;
     margin: 0;
+    padding-top: 20px;
     padding-left: 0;
     list-style-type: none;
   }
