0e7ee9531a2741613b159490c53caea88012d9f1
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-28 11:31:11 +0200

    added styles to the projectListCard itself

--- src/styles/ProjectListStyles.js
+++ src/styles/ProjectListStyles.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 
 export const ProjectListCardContainer = styled.div`
-  width: 100%;
+  width: 90%;
   height: 120px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border: 1px solid #e6e6e6;
+  border-top: 1px solid #f45c54;
   .projectListCard__title {
+    width: 30%;
+    height: 100%;
+  }
+  .projectListCard__info {
+    width: 30%;
+    height: 100%;
+    margin: 0;
+    padding-left: 0;
+    list-style-type: none;
   }
 `;
 
@@ -17,4 +26,5 @@ export const ProjectListCardImage = styled.div`
   background-image: ${(props) => `url(${props.backgroundImage})`};
   background-size: cover;
   background-position: center;
+  border-radius: 10px;
 `;
