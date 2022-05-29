de0fdea999ec8f58a2d1d09a533c046ce33b6394
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-29 17:03:03 +0200

    added the new reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import menuReducer from "./menuSlice";
+import projectsFilterReducer from "./projectsFilterSlice";
 
 const store = configureStore({
   reducer: {
     menu: menuReducer,
+    projectsFilter: projectsFilterReducer,
   },
 });
 
