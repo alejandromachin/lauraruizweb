b335560a0e425e62b51b7df23cca3f61727fb798
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-29 15:58:04 +0200

    added useEffect to set showMenu true when enter list page

--- src/pages/ProjectListPage/ProjectListPage.js
+++ src/pages/ProjectListPage/ProjectListPage.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ProjectList from "../../components/ProjectList/ProjectList";
 import projects from "../../data/projects";
+import { showMenuActionCreator } from "../../redux/menuSlice";
 
 const ProjectListPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showMenuActionCreator());
+  });
   return <ProjectList projects={projects} />;
 };
 export default ProjectListPage;
