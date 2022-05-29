f39634601d21c504cca730ff55d762ea6da04e0a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-29 16:00:57 +0200

    added useEffect to set showMenu true when enter home  page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ProjectsDisplay from "../../components/ProjectsDisplay/ProjectsDisplay";
 import projects from "../../data/projects";
+import { showMenuActionCreator } from "../../redux/menuSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showMenuActionCreator());
+  });
   return <ProjectsDisplay projects={projects} />;
 };
 export default Home;
