e1ff14f9a8de9ab11c5e52eca733921de25c54d9
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-30 14:52:18 +0200

    added links and styles to contactMenu

--- src/components/ContactMenu/ContactMenu.js
+++ src/components/ContactMenu/ContactMenu.js
@@ -1,9 +1,19 @@
 const ContactMenu = () => {
   return (
     <ul>
-      <li>INSTAGRAM</li>
-      <li>EMAIL</li>
-      <li>BARCELONA</li>
+      <li className="contactMenu__link">
+        <a
+          href="https://www.instagram.com/lauraruizpenacho/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          INSTAGRAM
+        </a>
+      </li>
+      <li className="contactMenu__link">
+        <a href="mailto: lruizpenacho@gmail.com">EMAIL</a>
+      </li>
+      <li>Based in Barcelona</li>
     </ul>
   );
 };
