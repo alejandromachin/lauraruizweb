96302e31f549a186d80bce38660cdd7f5509156e
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-05-31 15:15:21 +0200

    added hover styles only on the button hovering

--- src/styles/filterButton.js
+++ src/styles/filterButton.js
@@ -2,20 +2,28 @@ import styled from "styled-components";
 
 const FilterButtonContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   background-color: #d7c9e0;
   gap: 10px;
   padding-left: 20px;
   margin-top: 20px;
 
-  button {
-    width: 100px;
+  .current {
+    color: #000;
     border: 1px solid #000;
+    background-color: #a88dba;
+  }
+
+  button {
+    display: flex;
+    flex-direction: row;
+    width: 100px;
+    border: 0.5px solid #000;
     border-radius: 5px;
     background-color: #d7c9e0;
   }
-  button:hover {
-    background-color: #d7c9d0;
+  :hover > button:not(:hover) {
+    opacity: 0.5;
   }
 `;
 export default FilterButtonContainer;
