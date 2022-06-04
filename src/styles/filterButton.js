ac35f332a0a365e666f8f90fdefb0b1f6175ca06
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-06-04 11:58:41 +0200

    added transition and width to filter buttons

--- src/styles/filterButton.js
+++ src/styles/filterButton.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 const FilterButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
-  background-color: #d7c9e0;
+  width: 400px;
   gap: 10px;
   padding-left: 20px;
   margin-top: 20px;
+  z-index: 100;
 
   .current {
     color: #000;
@@ -14,16 +15,21 @@ const FilterButtonContainer = styled.div`
     background-color: #a88dba;
   }
 
-  button {
+  button,
+  button:active {
+    font-size: 12px;
     display: flex;
     flex-direction: row;
-    width: 100px;
+    align-items: center;
+    height: 20px;
     border: 0.5px solid #000;
     border-radius: 5px;
     background-color: #d7c9e0;
+    transition: 0.4s;
   }
   :hover > button:not(:hover) {
-    opacity: 0.5;
+    opacity: 0.2;
+    transition: 0.4s;
   }
 `;
 export default FilterButtonContainer;
