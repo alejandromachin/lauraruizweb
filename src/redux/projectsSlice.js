263c658828de507c44468e6e67f91d409a63cb42
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-06-07 22:18:26 +0200

    added a filter case in projectsSlice

--- src/redux/projectsSlice.js
+++ src/redux/projectsSlice.js
@@ -10,9 +10,17 @@ const projectsSlice = createSlice({
     setProjects: (state, action) => {
       state.projects = action.payload;
     },
+    filterProjects: (state, action) => {
+      state.projects = state.projects.filter(
+        (project) => project.category === action.payload
+      );
+    },
   },
 });
 
-export const { setProjects: setProjectsActionCreator } = projectsSlice.actions;
+export const {
+  setProjects: setProjectsActionCreator,
+  filterProjects: setFilterProjects,
+} = projectsSlice.actions;
 
 export default projectsSlice.reducer;
